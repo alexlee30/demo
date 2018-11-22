50a384448afff84e9a25f1c5046641ea15592f9f
alexlee30 <83508127@qq.com>
2018-11-22 09:41:30 +0800

    spring root & mybatis

--- src/main/java/com/example/demo/controller/AddressList.java
+++ src/main/java/com/example/demo/controller/AddressList.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
-@RequestMapping(value = "/Demo", method = { RequestMethod.GET, RequestMethod.POST })
+@RequestMapping(value = "/AddressList", method = { RequestMethod.GET, RequestMethod.POST })
 public class AddressList {
 
     @Autowired
